2f493830b91eea8d2be315909c528410fe22acbf
sriramk7-hk <sriramkesavan44@gmail.com>
2023-02-12 10:18:49 +0530

    Single Deploy Script

--- backend/deploy/01-deploy-pudhina.js
+++ backend/deploy/01-deploy-pudhina.js
@@ -28,6 +28,12 @@ module.exports = async({ getNamedAccounts, deployments}) => {
     logs: true
   })
 
+  const updateValue = await Pudhina.getNameAndSymbol("Dog", "DG");
+  await updateValue.wait(1);
+
+  const [_name, _symbol] = await Pudhina.returnNameAndSymbol();
+  console.log(`Updated Value ${_name}, ${_symbol}`);
+
   const mintNFT = await Pudhina.mintNft({value: mintFee.toString()});
   await mintNFT.wait(1);
 }
